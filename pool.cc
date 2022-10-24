40e0b87e869bdee44d4a5e9bbf1b562cc0a98147
Jim Liang <jimmyliang2001@gmail.com>
2022-10-24 18:45:58 -0400

    good start

--- pool.cc
+++ pool.cc
@@ -1,6 +1,9 @@
 #include "pool.h"
 #include <queue>
 #include <pthread.h>
+#include <mutex>
+#include <condition_variable>
+#include <unordered_map>
 
 Task::Task() {
 }
@@ -9,6 +12,12 @@ Task::~Task() {
 }
 
 ThreadPool::ThreadPool(int num_threads) {
+    //creates a conditional variable
+    //init to false cuz queue should start off false
+    std::condition_variable is_empty = true;
+    std::mutex lock;
+    //creates maps to keep track of the name
+    unordered_map<std::string, Task> names;
     //creates the usuable threads
     pthread_t *pool = new pthread_t[num_threads];
     for (int i = 0; i<num_threads; ++i) {
@@ -20,15 +29,29 @@ ThreadPool::ThreadPool(int num_threads) {
     
     //keeps track of free threads
     std::queue<Task> free_threads;
-
-
 }
 
 void ThreadPool::SubmitTask(const std::string &name, Task* task) {
+
+    task_queue.push(task);
+    names[name] = task;
+    is_empty.notify_all();
 }
 
 void ThreadPool::WaitForTask(const std::string &name) {
+    lk(&lock);
+    while(queue.empty()){
+        is_empty.wait(lock);
+    }
 }
 
 void ThreadPool::Stop() {
+    if (free_threads.size() != pool.size()){
+        delete(is_empty);
+        delete(lock);
+        delete(names);
+        delete(pool);
+        delete(task_queue);
+        delete(free_threads);
+    }
 }
