a6b19370cfd098d5d3a37dff1e2e182f823d0f08
Jim Liang <89550877+uvajim@users.noreply.github.com>
2022-10-24 15:05:56 -0400

    Add files via upload

---
+++ pool.cc
@@ -0,0 +1,20 @@
+#include "pool.h"
+
+Task::Task() {
+}
+
+Task::~Task() {
+}
+
+ThreadPool::ThreadPool(int num_threads) {
+    
+}
+
+void ThreadPool::SubmitTask(const std::string &name, Task* task) {
+}
+
+void ThreadPool::WaitForTask(const std::string &name) {
+}
+
+void ThreadPool::Stop() {
+}
