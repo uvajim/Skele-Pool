245ad8d11606d6079407c3383f142ad39d0e13d4
Jim Liang <jimmyliang2001@gmail.com>
2022-10-25 20:21:00 -0400

    redo

--- pool.cc
+++ pool.cc
@@ -4,6 +4,7 @@
 #include <mutex>
 #include <condition_variable>
 #include <unordered_map>
+#include <set>
 
 Task::Task() {
 }
@@ -12,46 +13,34 @@ Task::~Task() {
 }
 
 ThreadPool::ThreadPool(int num_threads) {
-    //creates a conditional variable
-    //init to false cuz queue should start off false
-    std::condition_variable is_empty = true;
-    std::mutex lock;
-    //creates maps to keep track of the name
-    unordered_map<std::string, Task> names;
-    //creates the usuable threads
-    pthread_t *pool = new pthread_t[num_threads];
+    //creates the thread pool
+    pool = new pthread_t[num_threads];
     for (int i = 0; i<num_threads; ++i) {
         pool[i] = i;
+        task_q.append(i);
     }
-
-    //initializes the task queue
-    std::queue<Task> task_queue;
-    
-    //keeps track of free threads
-    std::queue<Task> free_threads;
 }
 
 void ThreadPool::SubmitTask(const std::string &name, Task* task) {
-
-    task_queue.push(task);
+    prod_lk.lock();
+    q.append(task);
     names[name] = task;
-    is_empty.notify_all();
+    prod_lk.unlock();
+    Run()
+}
+
+void Run(){
+    consumer_lk.lock();
+    Task task = q.pop()
+    consumer_lk.unlock();
+    pthread_t* currThread = pool.pop();
+    pthread_t_create(currThread, NULL, task->Run, NULL);
+    completed_tasks.insert(task);
 }
 
 void ThreadPool::WaitForTask(const std::string &name) {
-    lk(&lock);
-    while(queue.empty()){
-        is_empty.wait(lock);
-    }
+
 }
 
 void ThreadPool::Stop() {
-    if (free_threads.size() != pool.size()){
-        delete(is_empty);
-        delete(lock);
-        delete(names);
-        delete(pool);
-        delete(task_queue);
-        delete(free_threads);
-    }
 }
--- pool.h
+++ pool.h
@@ -1,6 +1,11 @@
 #ifndef POOL_H_
 #include <string>
+#include <queue>
 #include <pthread.h>
+#include <mutex>
+#include <condition_variable>
+#include <unordered_map>
+#include <set>
 
 class Task {
 public:
@@ -20,8 +25,28 @@ public:
     // Wait for a task by name, if it hasn't been waited for yet. Only returns after the task is completed.
     void WaitForTask(const std::string &name);
 
+    void Run();
+
     // Stop all threads. All tasks must have been waited for before calling this.
     // You may assume that SubmitTask() is not caled after this is called.
     void Stop();
+
+    std::queue<Task*> q;
+
+    std::queue<int> thread_q;
+    //the set that will contain all completed _tasks
+    std::set<Task*> completed_tasks;
+    //keeps the name as a key, and the TaskID as a value
+    std::unordered_map<std::string, Task*> names;  
+    //creates the thread pool
+     pthread_t *pool;
+    //locks
+    //the lock for an empty queue
+    std::condition_variable is_empty;
+    //a simple lock for the producer
+    std::mutex prod_lk;
+    //a simple lock for the consumer
+    std::mutex consumer_lk;
+
 };
 #endif
